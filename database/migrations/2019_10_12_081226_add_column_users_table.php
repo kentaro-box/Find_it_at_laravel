1efec8d7decd90976eafd816915c66307a2bdd79
kentaro <>
2019-10-13 07:20:31 +0900

    first_commit

---
+++ database/migrations/2019_10_12_081226_add_column_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddColumnUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('company_name');
+            $table->string('phone_number');
+            $table->string('adress');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('company_name');
+            $table->dropColumn('phone_number');
+            $table->dropColumn('adress');
+        });
+    }
+}
