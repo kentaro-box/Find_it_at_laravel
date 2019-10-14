20ccd671c17ebdf692b99997b3f744d42b49fae0
vagrant <vagrant@find-it-at-laravel>
2019-10-14 07:19:03 +0000

    create_posts_table

---
+++ app/Post.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class Post extends Authenticatable
+{
+    protected $fillable = [
+        'title',
+        'body',
+        'image',
+        'deadline',
+        'material',
+        'amount', 
+        'frequency', 
+        'asking_price', 
+        'first_delivery_date', 
+        'getting_start', 
+        'finished_use', 
+        'user_id', 
+    ];
+
+}
